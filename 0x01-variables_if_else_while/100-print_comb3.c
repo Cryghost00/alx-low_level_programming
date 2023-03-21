5368f7b04af5147ee9739b5a69a3320b14ecfc0f
cryghost <ealdzy@gmail.com>
2023-03-20 19:15:47 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,4 @@
-#include <stdo.h>
+#include <stdio.h>
 /**
  * main - Main function
  *
@@ -8,10 +8,28 @@
 int main(void)
 {
 	int i;
+	int j;
+	int k;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		putchar(i + '0');
+		for (j = 1 ; j < 10 ; j++)
+		{
+			for (k = 2 ; k < 10 ; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (i + j + k != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
