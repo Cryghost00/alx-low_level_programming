a72b8004596d33d6054ed52e36bf26e927876df7
cryghost00 <ealdzy@gmail.com>
2023-12-23 07:29:40 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with the function puts
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	putss("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
